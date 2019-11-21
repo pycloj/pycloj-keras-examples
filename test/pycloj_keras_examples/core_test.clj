bd0e1b33630a98f5c06d741ea78d3484edfded45
david levy <davidvoler@gmail.com>
2019-11-21 15:31:25 +0200

    initial

---
+++ test/pycloj_keras_examples/core_test.clj
@@ -0,0 +1,7 @@
+(ns pycloj-keras-examples.core-test
+  (:require [clojure.test :refer :all]
+            [pycloj-keras-examples.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
