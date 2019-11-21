ffff9df7c9bb2eb3ee1fa2624efea6566e224520
daslu <daniel.slutsky@gmail.com>
2019-11-21 16:49:11 +0200

    whitespace

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"][pycloj/pycloj-keras "2.2.4-AUTO-0.1-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [pycloj/pycloj-keras "2.2.4-AUTO-0.1-SNAPSHOT"]]
   :main ^:skip-aot pycloj-keras-examples.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
