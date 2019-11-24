200384803300a40bf642282046c97436324000ea
daslu <daniel.slutsky@gmail.com>
2019-11-24 15:54:33 +0200

    updated dependencies

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [pycloj/pycloj-keras "2.2.4-AUTO-0.1-SNAPSHOT"]]
+                 [libpython-clj "1.6-SNAPSHOT"]
+                 [panthera "0.1-alpha.16"]
+                 #_[pycloj/pycloj-keras "2.2.4-AUTO-0.1-SNAPSHOT"]]
   :main ^:skip-aot pycloj-keras-examples.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/pycloj_keras_examples/core.clj
+++ src/pycloj_keras_examples/core.clj
@@ -1,10 +1,13 @@
 (ns  pycloj-keras-examples.core
   "Constrains the weights to be non-negative."
-  (:require [keras.Sequential :as Sequential]
+  (:require [keras]
+            [keras.Sequential :as Sequential :refer [Sequential]]
             [keras.Model :as Model]
             [keras.datasets.fashion-mnist :as fashion-mnist]
-            [keras.layers.Flatten :as Flatten]
-            [keras.layers.Dense :as Dense]))
+            [keras.layers.Flatten :as Flatten :refer [Flatten]]
+            [keras.layers.Dense :as Dense :refer [Dense]]
+            [libpython-clj.python :as py]))
+
 
 (defmacro def+
   "binding => binding-form
@@ -22,21 +25,38 @@
 
 (def+ [[train-images , train-labels], [test-images, test-labels]] (vec mnist-data))
 
-(def model (keras/Sequential [
-                           (layers/Flatten [] {:input_shape   [28,28]})
-                           (layers/Dense [128] {:activation  "relu"})
-                           (layers/Dense [10] {:activation "softmax" })]))
+(def f (Flatten [] {:input_shape [28,28]}))
+
+(def layers [f
+             ;; (Dense [128] {:activation "relu"})
+             ;; (Dense [10] {:activation "softmax"})
+             ])
+
+(def model (Sequential
+            :layers
+            layers))
+
+(Sequential/compile model
+                    :optimizer "adam"
+                    :loss      "sparse_categorical_crossentropy"
+                    :metrics   ["accuracy"])
+
+(py/get-attr f "input_shape")
+
+(keras.Sequential/fit model
+                      :x train-images
+                      :y train-labels
+                      :epochs 10)
+
+
+(let [f (Flatten [] {:input_shape [28,28]})]
+  (py/call-attr f "input_shape"))
 
 
 
-(keras.Sequntial/compile {:optimizer "adam"
-               :loss "sparse_categorical_crossentropy"
-               :metrics ["accuracy"]})
-
-(keras.Sequntial/fit  model train-images train-labels {:epochs 10})
 
 (defn -main
- 
+  
   "I don't do a whole lot ... yet."
   [& args]
 
